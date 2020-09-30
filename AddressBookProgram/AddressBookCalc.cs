08f9a63dd213bc1c00342b326fbeb1633b885656
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-01 00:06:06 +0530

    [mrunal] Add. Added feature to add Contact to address book

--- AddressBookProgram/AddressBookCalc.cs
+++ AddressBookProgram/AddressBookCalc.cs
@@ -10,5 +10,12 @@ namespace AddressBookProgram
     {
         public static List<PersonDetails> Contacts = new List<PersonDetails>();
 
+        public void AddContacts(string firstName, string lastName, string address, string city, string state, int zip, int phone, string email)
+        {
+            PersonDetails person = new PersonDetails(firstName, lastName, address, city, state, zip, phone, email);
+            Contacts.Add(person);
+            Console.WriteLine("Contact added successfully");
+        }
+
     }
 }
--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -7,7 +7,26 @@ namespace AddressBookProgram
         public static AddressBookCalc addressBook = new AddressBookCalc();
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program!\n");
+            Console.WriteLine("Welcome to Address Book Program!");
+            int option = 0;
+            while(option != 2)
+            {
+                Console.WriteLine("\nSelect an option : \n1. Add Contact \n2.Exit");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        ToAddContact();
+                        break;
+                    case 2:
+                        break;
+
+                }
+            }            
+        }
+
+        private static void ToAddContact()
+        {
             Console.Write("Enter First Name : ");
             var firstName = Console.ReadLine();
             Console.Write("Enter Last Name : ");
@@ -19,11 +38,13 @@ namespace AddressBookProgram
             Console.Write("Enter State : ");
             var state = Console.ReadLine();
             Console.Write("Enter Zip Code: ");
-            var zip = Console.ReadLine();
+            var zip = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter Phone Number : ");
-            var phone = Console.ReadLine();
+            var phone = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter Email : ");
             var email = Console.ReadLine();
+
+            addressBook.AddContacts(firstName, lastName, address, city, state, zip, phone, email);
         }
     }
 }
